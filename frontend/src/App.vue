1dfdce0d24ce7295c202fed08f5662cb93f33144
Lars Musters <lars.musters@ictu.nl>
2024-01-06 00:43:46 +0100

    init pixijs

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Application } from 'vue3-pixi'
+import { ref } from 'vue'
+
+import CursorSprite from './components/CursorSprite.vue'
+
+const enabled = ref(true)
+</script>
+
+<template>
+  <div style="margin-bottom: 10px">
+    <button @click="enabled = !enabled">Toggle</button>
+    <span style="margin-left: 10px"> Sprite visible: {{ enabled }} </span>
+  </div>
+  <Application :width="300" :height="300">
+    <CursorSprite v-if="enabled" :x="300 / 2" :y="300 / 2" />
+  </Application>
+</template>
+
+<style scoped></style>
